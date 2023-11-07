b7b582543f8e93411d50e4e4296865a906c664d5
peguygit config --global user.name peguygit config --global user.name peguy <peguynya@gmail.com>
2023-11-07 19:14:59 +0100

    Implemented the itShouldNotSaveNewCustomer test class

--- software-testing-peguy/software-testing/src/test/java/com/peguy/softwaretesting/customer/CustomerRegistrationServiceTest.java
+++ software-testing-peguy/software-testing/src/test/java/com/peguy/softwaretesting/customer/CustomerRegistrationServiceTest.java
@@ -14,8 +14,10 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
 
 
 class CustomerRegistrationServiceTest {
@@ -51,6 +53,44 @@ class CustomerRegistrationServiceTest {
         assertThat(customer1).isEqualTo(customer);
         assertThat(customer1.getName()).isEqualTo("Peguy");
         assertThat(customer1.getPhoneNumber()).isEqualTo(phone_number);
+
+    }
+
+    @Test
+    void itShouldNotSaveTheCustomerWhenPhoneNumberIsAlreadyAssigned() {
+        //Given
+        UUID customerId = UUID.randomUUID();
+        String phone_number ="3298711186";
+        Customer customer = new Customer(customerId, "Peguy", phone_number);
+        Customer fakeCustomer = new Customer(UUID.randomUUID(), "Diaba", phone_number);
+        CustomerRegistrationRequest customerRegistrationRequest = new CustomerRegistrationRequest(customer);
+        given(customerRegistrationRepo.selectCustomerByPhoneNumber(phone_number)).willReturn(Optional.of(fakeCustomer));
+
+        //When
+        customerRegistrationService.registerNewCustomer(customerRegistrationRequest);
+
+        //Then
+        then(customerRegistrationRepo).should(never()).save(any());
+        then(customerRegistrationRepo).should().selectCustomerByPhoneNumber(phone_number);
+        then(customerRegistrationRepo).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    void itShouldNotSaveAndExistingCustomer() {
+        //Given
+        UUID customerId = UUID.randomUUID();
+        String phone_number ="3298711186";
+        Customer customer = new Customer(customerId, "Peguy", phone_number);
+        CustomerRegistrationRequest customerRegistrationRequest = new CustomerRegistrationRequest(customer);
+        given(customerRegistrationRepo.selectCustomerByPhoneNumber(phone_number)).willReturn(Optional.of(customer));
+
+        //When
+        customerRegistrationService.registerNewCustomer(customerRegistrationRequest);
+
+        //Then
+        then(customerRegistrationRepo).should().selectCustomerByPhoneNumber(phone_number);
+        then(customerRegistrationRepo).shouldHaveNoMoreInteractions();
+        then(customerRegistrationRepo).should(never()).save(any());
 
 
     }
